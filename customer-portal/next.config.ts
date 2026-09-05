464108e16d1106ed568871fb329b057b232857d0
Mike Holownych <mike.holownych@aisyndicate.io>
2026-09-05 21:53:33 +0000

    fix: allow Opinly telemetry connection

--- customer-portal/next.config.ts
+++ customer-portal/next.config.ts
@@ -243,7 +243,7 @@ const nextConfig: NextConfig = {
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: https://indieascent.com https://nicklaunches.com https://www.googletagmanager.com https://www.google-analytics.com https://www.google.com https://getvisibai.com https://static.peerpush.com https://maidensail.com",
               "font-src 'self' data:",
-              "connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://us.posthog.com https://us.i.posthog.com https://cloudflareinsights.com https://searchable-tracker.searchable.workers.dev https://tracker.searchableanalytics.com https://in.heycatch.ai https://news.google.com",
+              "connect-src 'self' https://www.google-analytics.com https://www.googletagmanager.com https://www.google.com https://us.posthog.com https://us.i.posthog.com https://cloudflareinsights.com https://searchable-tracker.searchable.workers.dev https://tracker.searchableanalytics.com https://in.heycatch.ai https://news.google.com https://static.opinly.ai",
               "frame-src https://news.google.com",
               "object-src 'none'",
               "base-uri 'self'",
